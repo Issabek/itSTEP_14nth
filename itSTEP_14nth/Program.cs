88162e5a482ff0f30779d065df64493aa68c68f4
Issabek <180107287@stu.sdu.edu.kz>
2021-04-05 16:18:33 +0600

    done

---
+++ itSTEP_14nth/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace itSTEP_14nth
+{
+    class Program
+    {
+        private delegate void stepTaskHandler(string s);
+
+        static void Main()
+        {
+            stepTaskHandler del = Method1;
+            del += Method2;
+
+            foreach (stepTaskHandler nextDel in del.GetInvocationList())
+            {
+                try
+                {
+                    nextDel.Invoke("Yooper");
+                }
+                catch (Exception xx)
+                {
+                    Console.WriteLine(string.Format("Exception in {0}: {1}", nextDel.Method.Name, xx.Message));
+                }
+            }
+        }
+
+        static void Method1(string text)
+        {
+            throw new Exception(string.Format("Problem in Method1: {0}",text));
+        }
+
+        static void Method2(string name)
+        {
+            Console.WriteLine("Method2, {0}", name);
+        }
+    }
+
+}
+
